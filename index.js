70e485cdfb0ee7b5b97197de5505ba8eb8fa0464
Ahmad Hariri <ahmadmhd357@gmail.com>
2022-10-09 14:19:20 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let arr=["sName","sName","evName "];
+function writeCards(sName,evName) {
+    let newArr=[];
+    for ( let i = 0; i < arr.length; i++) {
+      newArr.push(`Thank you, ${sName[i]}, for the wonderful ${evName} gift!` );
+    };
+  
+    return newArr;
+  }
+  
+  function countDown(num){
+    
+    while(num >= 0 ){
+     console.log(num );
+      
+      num--;
+    };
+    
+  }
+  countDown(10);
+  
